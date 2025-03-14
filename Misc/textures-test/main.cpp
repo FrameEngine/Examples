85730e055dbfdf4e471561de8bec317baad65e78
Perchinka <alexsator.lukin@gmail.com>
2025-03-14 15:32:37 +0000

    Add textures-test

---
+++ Misc/textures-test/main.cpp
@@ -0,0 +1,78 @@
+#include "FrameEngine.hpp"
+using namespace FrameEngine;
+
+class Simulation : public Engine {
+private:
+  Mesh *cubeMesh;
+  Mesh *sphereMesh;
+
+  Object *cubeObject;
+  Object *sphereObject;
+
+  PointLight *pointLight1;
+  PointLight *pointLight2;
+
+  Texture2D *texture;
+
+  float timeElapsed = 0.0f;
+
+public:
+  void on_start() override {
+    Shader *lightingShader =
+        new FrameEngine::Shader("shaders/basic.vs", "shaders/basic.fs");
+
+    texture = new Texture2D();
+    if (!texture->loadFromFile("assets/wood_texture.jpg")) {
+      LOG(ERROR, "Failed to load texture!");
+    }
+
+    cubeMesh = MeshGenerator::createCube();
+    sphereMesh = MeshGenerator::createSphere();
+
+    cubeObject = new Object(cubeMesh);
+    cubeObject->setMesh(cubeMesh);
+
+    BasicMaterial *redMat = new FrameEngine::BasicMaterial(
+        lightingShader, Vector3(1, 0, 0), Vector3(1, 1, 1), 64.0f);
+
+    cubeObject->setMaterial(redMat);
+    cubeObject->transform->position = Vector3(-2.0f, 0.0f, 0.0f);
+
+    sphereObject = new Object(sphereMesh);
+    sphereObject->setMesh(sphereMesh);
+
+    BasicMaterial *blueMat = new FrameEngine::BasicMaterial(
+        lightingShader, Vector3(1, 1, 1), Vector3(1, 1, 1), 32.0f);
+    blueMat->setTexture(texture);
+
+    sphereObject->setMaterial(blueMat);
+    sphereObject->transform->position = Vector3(2.0f, 0.0f, 0.0f);
+    sphereObject->transform->scale *= 3.f;
+
+    pointLight1 = new PointLight(Vector3(-5.0f, 5.0f, 5.0f),
+                                 Vector3(1.0f, 1.0f, 1.0f), 3.f);
+
+    Camera &camera = renderer.getCamera();
+    camera.transform->position = Vector3(0.0f, 3.0f, -10.0f);
+    camera.setProjection(60.0f, 16.0f / 9.0f, 0.1f, 100.0f);
+
+    camera.lookAt(Vector3(0, 0, 0));
+  }
+
+  void fixed_update(float dt) override {
+    timeElapsed += dt;
+
+    pointLight1->transform->position =
+        Vector3(5.0f * sin(timeElapsed), 3, 5 * cos(timeElapsed));
+
+    cubeObject->rotate(Vector3(0, 1, 0), 0.5f);
+    sphereObject->rotate(Vector3(0, 1, 0), -0.5f);
+  }
+};
+
+int main() {
+  Simulation sim;
+  sim.init();
+  sim.run();
+  return 0;
+}
---
+++ Misc/textures-test/shaders/basic.fs
@@ -0,0 +1,72 @@
+#version 450 core
+#define MAX_LIGHTS 10
+
+struct LightData {
+    vec4 position;   // xyz: world-space position 
+    vec4 color;      // rgb: light color, w: intensity
+};
+
+layout(std140, binding = 0) uniform LightBlock {
+    int numLights;
+    LightData lights[MAX_LIGHTS];
+};
+
+uniform vec3 ambientColor;
+uniform mat4 model;
+uniform mat4 view;
+uniform mat4 projection;
+uniform vec3 viewPos;
+
+struct Material {
+    vec3 diffuseColor;
+    vec3 specularColor;
+    float specularPower;
+};
+uniform Material material;
+
+uniform sampler2D texSampler;
+
+in vec3 FragPos;
+in vec3 Normal;
+in vec2 TexCoords;
+
+out vec4 FragColor;
+
+void main() {
+    vec3 norm = normalize(Normal);
+    vec3 viewDir = normalize(viewPos - FragPos);
+    
+    vec3 ambient = ambientColor * material.diffuseColor;
+    
+    vec3 diffuseTotal = vec3(0.0);
+    vec3 specularTotal = vec3(0.0);
+    
+    for (int i = 0; i < numLights; i++) {
+        vec3 lightPos = lights[i].position.xyz;
+        vec3 lightColor = lights[i].color.rgb;
+        float intensity = lights[i].color.w;
+        
+        vec3 lightDir = normalize(lightPos - FragPos);
+        
+        float diff = max(dot(norm, lightDir), 0.0);
+        vec3 diffuse = diff * lightColor * material.diffuseColor;
+        
+        vec3 reflectDir = reflect(-lightDir, norm);
+        float spec = 0.0;
+        if (diff > 0.0) {
+            spec = pow(max(dot(viewDir, reflectDir), 0.0), material.specularPower);
+        }
+        vec3 specular = spec * lightColor * material.specularColor;
+        
+        float distance = length(lightPos - FragPos);
+        float attenuation = 1.0 / (1.0 + 0.09 * distance + 0.032 * (distance * distance));
+        
+        diffuseTotal += diffuse * intensity * attenuation;
+        specularTotal += specular * intensity * attenuation;
+    }
+    
+    vec3 texColor = texture(texSampler, TexCoords).rgb;
+    vec3 finalColor = (ambient + diffuseTotal + specularTotal) * texColor;
+    FragColor = vec4(finalColor, 1.0);
+}
+
